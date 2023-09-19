22a31c61217330f147dde0ed29dd16eddd6f773a
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-19 21:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
+let arr = Object.keys(sampleObject);
 function hasKey(key) {
   //   write your code here
-	if(sampleObject.contains(key)){
-		return true;
-	}
-	else return false;
+  for (let i = 0; i < arr.length; i++) {
+    if (key === arr[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // Do not change the code belo
